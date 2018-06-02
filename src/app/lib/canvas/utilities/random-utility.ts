3c314cb930828822f01926580fcb773164d2e1a3
T <thaynie@gmail.com>
2018-06-02 13:40:20 -0500

    baby steps

--- src/app/lib/canvas/utilities/random-utility.ts
+++ src/app/lib/canvas/utilities/random-utility.ts
@@ -1,4 +1,5 @@
-export class RandomUtility{
+import { iPoint } from '../interfaces/iPoint';
+export class RandomUtility {
     /// returns a random number between -1 and 1
     randomWithNegative() {
         return (((Math.random() * 200) - 100) / 100);
@@ -7,5 +8,11 @@ export class RandomUtility{
     /// returns a random number between n1 and n2
     randomNumberBetween(n1, n2) {
         return Math.floor(Math.random() * Math.max(n1, n2)) + Math.min(n1, n2);
+    }
+
+    randomPointInBounds(w: number, h: number): iPoint {
+        let rx = Math.random() * w;
+        let ry = Math.random() * h;
+        return <iPoint>{ x: rx, y: ry };
     }
 }
--- src/app/scenes/scene02/scene02.component.ts
+++ src/app/scenes/scene02/scene02.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { CanvasWrapper } from '../../lib/canvas/canvas-wrapper';
+import { iRectangle } from '../../lib/canvas/interfaces/iRectangle';
+import { iCircle } from '../../lib/canvas/interfaces/iCircle';
 
 @Component({
   selector: 'app-scene02',
@@ -10,6 +12,8 @@ export class Scene02Component implements OnInit {
   @ViewChild('c') canvasRef: ElementRef;
   private cw: CanvasWrapper;
 
+  private squares: iRectangle[] = [];
+
   constructor() { }
 
   ngOnInit() {
@@ -17,6 +21,7 @@ export class Scene02Component implements OnInit {
     this.cw.panZoomManager.minScale = 1;
     this.cw.panZoomManager.panningAllowed = false;
 
+    this.generateSquares();
     // start the draw loop
     this.cw.start();
   }
@@ -24,11 +29,46 @@ export class Scene02Component implements OnInit {
   draw() {
     this.cw.saveContext();
 
-    // do some cool stuff
-    // ...
-    //
+    this.doSomething();
 
     this.cw.restoreContext();
+  }
+
+  generateSquares() {
+    for (let x = 0; x < 10; x++) {
+      this.squares.push(<iRectangle>{
+        point: this.cw.randoms.randomPointInBounds(this.cw.width, this.cw.height),
+        size: { width: 30, height: 30 },
+        color: { color: '#888', alpha: 1 }
+      });
+    }
+  }
+
+  doSomething() {
+    this.drawSquares();
+    this.drawMousePosition();
+  }
+
+  drawSquares() {
+    this.squares.forEach(square => {
+      this.cw.shapes.drawRectangle(square);
+    });
+  }
+
+  drawMousePosition() {
+    let mm = this.cw.mouseManager;
+    if (!mm.mouseOffCanvas) {
+      let mp = <iCircle>{
+        point: { x: mm.mouseX, y: mm.mouseY },
+        radius: 2,
+        color: {
+          alpha: 1,
+          color: 'red'
+        }
+      };
+
+      this.cw.shapes.drawCircle(mp);
+    }
   }
 
 }
