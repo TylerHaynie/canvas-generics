9f54edea39ad0a059ddb8e838b55329eab402251
T <thaynie@gmail.com>
2018-06-11 18:17:26 -0500

    removed private

--- src/app/lib/canvas/objects/vector.ts
+++ src/app/lib/canvas/objects/vector.ts
@@ -1,17 +1,17 @@
 import { vec2 } from 'gl-matrix';
 
 export class Vector2D {
-    private _vector: vec2;
+    vector: vec2;
 
     public get x() {
-        return <number>this._vector[0];
+        return <number>this.vector[0];
     }
     public get y() {
-        return <number>this._vector[1];
+        return <number>this.vector[1];
     }
 
     constructor(x: number, y: number) {
-        this._vector = vec2.fromValues(x, y);
+        this.vector = vec2.fromValues(x, y);
     }
 
 }
